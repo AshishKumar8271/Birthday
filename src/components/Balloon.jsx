cb96fd6874ca7e5e542ec5379dee7b160482683a
sachinDev07 <sachinkrj077@gmail.com>
2024-02-10 18:07:57 +0530

    Fixed css of balloons

---
+++ src/components/Balloon.jsx
@@ -0,0 +1,280 @@
+import { motion } from "framer-motion";
+
+import ballon1 from "../assets/img/ballon3.svg";
+import ballon2 from "../assets/img/ballon2.svg";
+import ballon3 from "../assets/img/ballon3.svg";
+
+const Balloon = () => {
+  return (
+    <div className="seven">
+      <div className="baloons">
+        <motion.img
+          initial={{ y: 550 }}
+          animate={{
+            y: [550, 530, 550],
+            transition: { duration: 3, repeat: Infinity },
+          }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          src={ballon2}
+          alt=""
+        />
+        <motion.img
+          initial={{ y: 650 }}
+          animate={{
+            y: [60, 40, 60],
+            transition: { duration: 3, repeat: Infinity },
+          }}
+          transition={{ duration: 2, ease: "easeInOut" }}
+          src={ballon1}
+          alt=""
+        />
+        <motion.img
+          initial={{ y: 670 }}
+          animate={{
+            y: [550, 530, 550],
+            transition: { duration: 3, repeat: Infinity },
+          }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          src={ballon3}
+          alt=""
+        />
+        <motion.img
+          initial={{ y: 700 }}
+          animate={{ y: -700 }}
+          transition={{ duration: 2, ease: "easeInOut" }}
+          src={ballon1}
+          alt=""
+        />
+        <motion.img
+          initial={{ y: 700 }}
+          animate={{ y: -700 }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          src={ballon2}
+          alt=""
+        />
+        <motion.img
+          initial={{ y: 700 }}
+          animate={{ y: -700 }}
+          transition={{ duration: 2, ease: "easeInOut" }}
+          src={ballon3}
+          alt=""
+        />
+        <motion.img
+          initial={{ y: 700 }}
+          animate={{
+            y: [50, 30, 50],
+            transition: { duration: 3, repeat: Infinity },
+          }}
+          transition={{ duration: 2, ease: "easeInOut" }}
+          src={ballon2}
+          alt=""
+        />
+        <motion.img
+          initial={{ y: 700 }}
+          animate={{
+            y: [80, 60, 80],
+            transition: { duration: 3, repeat: Infinity },
+          }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          src={ballon3}
+          alt=""
+        />
+        <motion.img
+          initial={{ y: 700 }}
+          animate={{ y: -700 }}
+          transition={{ duration: 2, ease: "easeInOut" }}
+          src={ballon1}
+          alt=""
+        />
+        <motion.img
+          initial={{ y: 700 }}
+          animate={{
+            y: [550, 530, 550],
+            transition: { duration: 3, repeat: Infinity },
+          }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          src={ballon2}
+          alt=""
+        />
+        <motion.img
+          initial={{ y: 700 }}
+          animate={{
+            y: [750, 730, 750],
+            transition: { duration: 3, repeat: Infinity },
+          }}
+          transition={{ duration: 2, ease: "easeInOut" }}
+          src={ballon3}
+          alt=""
+        />
+        <motion.img
+          initial={{ y: 700 }}
+          animate={{ y: -700 }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          src={ballon2}
+          alt=""
+        />
+        <motion.img
+          initial={{ y: 700 }}
+          animate={{ y: -700 }}
+          transition={{ duration: 2, ease: "easeInOut" }}
+          src={ballon1}
+          alt=""
+        />
+        <motion.img
+          initial={{ y: 700 }}
+          animate={{ y: -700 }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          src={ballon3}
+          alt=""
+        />
+        <motion.img
+          initial={{ y: 700 }}
+          animate={{
+            y: [-20, -10, -20],
+            transition: { duration: 3, repeat: Infinity },
+          }}
+          transition={{ duration: 2, ease: "easeInOut" }}
+          src={ballon2}
+          alt=""
+        />
+        <motion.img
+          initial={{ y: 700 }}
+          animate={{ y: -700 }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          src={ballon3}
+          alt=""
+        />
+        <motion.img
+          initial={{ y: 700 }}
+          animate={{ y: -700 }}
+          transition={{ duration: 2, ease: "easeInOut" }}
+          src={ballon1}
+          alt=""
+        />
+        <motion.img
+          initial={{ y: 700 }}
+          animate={{ y: -700 }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          src={ballon2}
+          alt=""
+        />
+        <motion.img
+          initial={{ y: 700 }}
+          animate={{ y: -700 }}
+          transition={{ duration: 2, ease: "easeInOut" }}
+          src={ballon1}
+          alt=""
+        />
+        <motion.img
+          initial={{ y: 700 }}
+          animate={{ y: -700 }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          src={ballon3}
+          alt=""
+        />
+        <motion.img
+          initial={{ y: 700 }}
+          animate={{ y: -700 }}
+          transition={{ duration: 2, ease: "easeInOut" }}
+          src={ballon3}
+          alt=""
+        />
+        <motion.img
+          initial={{ y: 700 }}
+          animate={{ y: -700 }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          src={ballon1}
+          alt=""
+        />
+        <motion.img
+          initial={{ y: 700 }}
+          animate={{ y: -700 }}
+          transition={{ duration: 2, ease: "easeInOut" }}
+          src={ballon2}
+          alt=""
+        />
+        <motion.img
+          initial={{ y: 700 }}
+          animate={{ y: -700 }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          src={ballon3}
+          alt=""
+        />
+        <motion.img
+          initial={{ y: 700 }}
+          animate={{ y: -700 }}
+          transition={{ duration: 2, ease: "easeInOut" }}
+          src={ballon2}
+          alt=""
+        />
+        <motion.img
+          initial={{ y: 700 }}
+          animate={{ y: -700 }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          src={ballon1}
+          alt=""
+        />
+        <motion.img
+          initial={{ y: 700 }}
+          animate={{ y: -700 }}
+          transition={{ duration: 2, ease: "easeInOut" }}
+          src={ballon3}
+          alt=""
+        />
+        <motion.img
+          initial={{ y: 700 }}
+          animate={{
+            y: [50, 30, 50],
+            transition: { duration: 3, repeat: Infinity },
+          }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          src={ballon2}
+          alt=""
+        />
+        <motion.img
+          initial={{ y: 700 }}
+          animate={{ y: -700 }}
+          transition={{ duration: 2, ease: "easeInOut" }}
+          src={ballon3}
+          alt=""
+        />
+        <motion.img
+          initial={{ y: 700 }}
+          animate={{ y: -700 }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          src={ballon1}
+          alt=""
+        />
+        <motion.img
+          initial={{ y: 700 }}
+          animate={{
+            y: [50, 30, 50],
+            transition: { duration: 3, repeat: Infinity },
+          }}
+          transition={{ duration: 2, ease: "easeInOut" }}
+          src={ballon2}
+          alt=""
+        />
+        <motion.img
+          initial={{ y: 700 }}
+          animate={{ y: -700 }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          src={ballon1}
+          alt=""
+        />
+        <motion.img
+          initial={{ y: 700 }}
+          animate={{
+            y: [50, 30, 50],
+            transition: { duration: 3, repeat: Infinity },
+          }}
+          transition={{ duration: 2, ease: "easeInOut" }}
+          src={ballon3}
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Balloon;
